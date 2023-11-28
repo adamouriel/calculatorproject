cadd00899a8ca3fe7e529024f4867349b49b3844
adamouriel <147005707+adamouriel@users.noreply.github.com>
2023-11-27 21:41:52 -0500

    progress

--- script.js
+++ script.js
@@ -3,11 +3,65 @@ let secondNumber
 let step = 0
 let operation
 let result = 0
-
 let numArray = []
 let numArray2 = []
+const keys = {
+    '+': 107,
+    '-': 109,
+    '*': 106,
+    '/': 111,
+    '=': 13,
+    'delete': 8,
+};
+
+const powerLight = document.querySelector('.power-light');
+
+document.addEventListener('click', turnOn);
+document.addEventListener('keydown', turnOn);
+
+function turnOn() {
+  powerLight.classList.add('on');
+}
 
 const display = document.getElementById('display')
+
+document.addEventListener('keydown', handleInput);
+document.addEventListener('keydown', handleKeys);
+
+function handleInput(e) {
+    if (!isNaN(e.key)) {
+        getNum(e.key)
+    }
+}
+
+function handleKeys(e) {
+    let key = keys[e.keyCode];
+    if (key == 'delete') {
+        let currentVal = display.value
+        currentVal = currentVal.slice(0, -1)
+        display.value = currentVal
+    } else if (key == '=') {
+        calcEq();
+    } else {
+        operate(key);
+    }
+}
+
+function operate(oper) {
+    if (oper == '+') {
+        result = firstNumber + secondNumber
+        display.value = result
+    } else if (oper == '-') {
+        result = firstNumber - secondNumber
+        display.value = result
+    } else if (oper == '*') {
+        result = firstNumber * secondNumber
+        display.value = result
+    } else if (oper == '/') {
+        result = firstNumber / secondNumber
+        display.value = result
+    }
+}
 
 function getNum(num) {
     if (step === 0 || step === 1) {
@@ -23,8 +77,14 @@ function getNum(num) {
 }
 
 function getOp(op) {
-    step = 2
-    operation = op
+    if (op === 'del') {
+        let currentVal = display.value
+        currentVal = currentVal.slice(0, -1)
+        display.value = currentVal
+    } else {
+        step = 2
+        operation = op
+    }
 }
 
 function allClear() {
@@ -38,17 +98,21 @@ function allClear() {
     numArray2 = []
 }
 
-const calcEq = () => {
-    if (operation === '+') {
-        result = firstNumber + secondNumber;
-        display.value = result;
-    } else if (operation === '-'){
-        result = firstNumber - secondNumber;
-        display.value = result;
-    } else if (operation === '*'){
+function calcEq() {
+    if (operation === 'del') {
+        let currentVal = display.value
+        currentVal = currentVal.slice(0, -1)
+        display.value = currentVal
+    } else if (operation === '+') {
+        result = firstNumber + secondNumber
+        display.value = result
+    } else if (operation === '-') {
+        result = firstNumber - secondNumber
+        display.value = result
+    } else if (operation === '*') {
         result = firstNumber * secondNumber;
         display.value = result;
-    } else if (operation === "/"){
+    } else if (operation === "/") {
         result = firstNumber / secondNumber;
         display.value = result;
     }
